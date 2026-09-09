aade5659caab0f27aa8c7acd2fbde12b9356e5b5
usamaarain787878-lab <usamaarain787878@gmail.com>
2026-09-09 12:50:59 +0500

    Add generate_dataset.py for dataset creation
    Added generate_dataset.py for dataset generation.

---
+++ generate_dataset.py
@@ -0,0 +1,10 @@
+Telco-Customer-Churn-Prediction-ANN/
+│
+├── generate_dataset.py   ← dataset banane ka code
+├── Cleaned_Dataset.csv
+├── app.py
+├── model_performance.py
+│
+└── models/
+    ├── churn_model.keras  ← BAAD MEIN ANN training ke baad banegi
+    └── scaler.pkl         ← BAAD MEIN scaler save hoga
